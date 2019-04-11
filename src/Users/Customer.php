d04f5fb605d801e59a22c65f1423797a7e4244f2
Tobias Thaden <tt@convoyinteractive.com>
2019-04-11 14:55:51 +0200

    commit

---
+++ src/Users/Customer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Marktstand\Users;
+
+use Marktstand\Checkout\Cart;
+use Marktstand\Access\Verifiable;
+use Marktstand\Company\HasCompany;
+use Marktstand\Support\Reflectable;
+use Illuminate\Foundation\Auth\User;
+use Marktstand\Payment\HasBankAccounts;
+
+class Customer extends User
+{
+    use HasBankAccounts, HasCompany, Reflectable, Verifiable;
+
+    /**
+     * @var string
+     */
+    public $type = 'customer';
+
+    /**
+     * The customers cart.
+     */
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    /**
+     * Get the users state.
+     *
+     * @return Marktstand\Users\State
+     */
+    public function state()
+    {
+        return new State($this);
+    }
+}
