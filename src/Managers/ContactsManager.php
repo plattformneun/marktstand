85ed5f91138766c054f785f6cba4882b3338ded6
Tobias Thaden <tt@convoyinteractive.com>
2019-07-04 22:05:12 +0200

    commit

---
+++ src/Managers/ContactsManager.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Marktstand\Managers;
+
+use Marktstand\Company\Contact;
+
+class ContactsManager extends Manager
+{
+    /**
+     * Create a new contact.
+     *
+     * @param array $data
+     * @param Illuminate\Foundation\Auth\User $user
+     * @return Marktstand\Company\Contact
+     */
+    public function create(array $data, User $user)
+    {
+        $contact = new Contact;
+
+        $this->makeFillable($contact)->fill(array_merge($data, [
+            'user_id' => $user->id,
+            'user_type' => $user->type,
+        ]))->save();
+
+        return $contact;
+    }
+
+    /**
+     * Get the users contacts.
+     *
+     * @param  Illuminate\Foundation\Auth\User $user
+     * @return Illuminate\Support\Collection
+     */
+    public function fromUser(User $user)
+    {
+        return $user->contacts;
+    }
+
+    /**
+     * Find the contact by id.
+     *
+     * @param  int $id
+     * @return Marktstand\Company\Contact
+     */
+    public function fromId($id)
+    {
+        return Contact::findOrFail($id);
+    }
+
+    /**
+     * Update the given contact.
+     *
+     * @param  Marktstand\Company\Contact $contact
+     * @param  array $data
+     * @return Marktstand\Company\Contact
+     */
+    public function update(Contact $contact, array $data)
+    {
+        $this->makeFillable($contact)->update($data);
+
+        return $contact;
+    }
+
+    /**
+     * Define the fillable fields.
+     *
+     * @return array
+     */
+    protected function fillable()
+    {
+        return [
+            'position', 'gender', 'firstname', 'lastname', 'email', 'user_id', 'user_type',
+        ];
+    }
+}
---
+++ tests/Product/ProductTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Marktstand\Tests\Product;
+
+use Marktstand\Product\Filter;
+use Marktstand\Tests\TestCase;
+use Marktstand\Users\Producer;
+use Marktstand\Product\Product;
+use Marktstand\Product\Quality;
+use Marktstand\Product\Category;
+use Marktstand\Events\ProductSaved;
+use Illuminate\Support\Facades\Event;
+
+class ProductTest extends TestCase
+{
+    /** @test */
+    public function it_fires_the_saved_event()
+    {
+        Event::fake();
+
+        factory(Product::class)->create();
+
+        Event::assertDispatched(ProductSaved::class);
+    }
+
+    /** @test */
+    public function it_has_a_producer()
+    {
+        $producer = factory(Producer::class)->create();
+        $product = factory(Product::class)->create(['producer_id' => $producer->id]);
+
+        $this->assertEquals($producer->id, $product->producer->id);
+    }
+
+    /** @test */
+    public function it_belongs_to_categories()
+    {
+        $category = factory(Category::class)->create();
+        $product = factory(Product::class)->create();
+
+        $product->categories()->attach([$category->id]);
+
+        $this->assertDatabaseHas('category_product', [
+            'category_id' => $category->id,
+            'product_id' => $product->id,
+        ]);
+
+        $this->assertCount(1, $product->categories);
+    }
+
+    /** @test */
+    public function it_belongs_to_filters()
+    {
+        $filter = factory(Filter::class)->create();
+        $product = factory(Product::class)->create();
+
+        $product->filters()->attach([$filter->id]);
+
+        $this->assertDatabaseHas('filter_product', [
+            'filter_id' => $filter->id,
+            'product_id' => $product->id,
+        ]);
+
+        $this->assertCount(1, $product->filters);
+    }
+
+    /** @test */
+    public function it_belongs_to_qualities()
+    {
+        $quality = factory(Quality::class)->create();
+        $product = factory(Product::class)->create();
+
+        $product->qualities()->attach([$quality->id]);
+
+        $this->assertDatabaseHas('qualifyables', [
+            'quality_id' => $quality->id,
+            'qualifyable_id' => $product->id,
+            'qualifyable_type' => 'product',
+        ]);
+
+        $this->assertCount(1, $product->qualities);
+
+        $this->assertCount(1, $quality->products);
+    }
+}
