c6b7c3a95711714a4a2469e51470e277d4524a3b
Tobias Thaden <hello@tobiasthaden.com>
2019-09-04 17:08:02 +0200

    paginate producers

--- src/Managers/ProducersManager.php
+++ src/Managers/ProducersManager.php
@@ -7,6 +7,40 @@ use Marktstand\Users\Producer;
 class ProducersManager extends Manager
 {
     /**
+     * Pagination count.
+     *
+     * @var int
+     */
+    protected $paginate;
+
+    /**
+     * The related models that should be eager loaded.
+     *
+     * @return self
+     */
+    public function paginate($count)
+    {
+        $this->paginate = $count;
+
+        return $this;
+    }
+
+    /**
+     * Fetch all producers.
+     *
+     * @param  array $with
+     * @return Marktstand\Users\Producer
+     */
+    public function all(array $with = [])
+    {
+        if ($this->paginate) {
+            return Producer::with($with)->paginate($this->paginate);
+        }
+        
+        return Producer::with($with)->get();
+    }
+
+    /**
      * Find a producer by username.
      *
      * @param  string $username
